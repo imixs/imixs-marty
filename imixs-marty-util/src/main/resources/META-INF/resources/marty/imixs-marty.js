07b1b45d50f123f86cca4ddacb9b5e5f4cc5684f
Ralph Soika <ralph.soika@imixs.com>
2013-04-24 08:40:59 +0000

    merge new version 2.0

---
+++ imixs-marty-util/src/main/resources/META-INF/resources/marty/imixs-marty.js
@@ -0,0 +1,35 @@
+/* This function is to deleay the blur event for the SuggestInput fields a little bit, 
+ * so the commandlink event can be fired before. 
+ * This method is used by teh worktiemLink.xhtml
+ * See: http://stackoverflow.com/questions/12677179/delay-a-jsf-ajax-listener-for-checkbox-group 
+ * 
+ * The method also clears the input value on blur event.
+ */
+$(document).ready(function() {
+	$(".suggestinput").each(function(index, input) {
+	    var onblur = input.onblur;
+	    input.onblur = null;	
+	    $(input).on("blur", function(event) {
+	    	// clear the input on blur
+	    	$(this).val(''); 
+	        delayEvent(function() { onblur.call(input, event); }, 300);
+	    });
+	    
+	    // turn autocomplete of
+	    $(this).attr('autocomplete','off');
+	});
+
+});
+
+var delayEvent = (function() {
+	
+    var timer = 0;
+    return function(callback, timeout) {
+        clearTimeout(timer);
+        timer = setTimeout(callback, timeout);
+    };
+})();
+
+
+
+
