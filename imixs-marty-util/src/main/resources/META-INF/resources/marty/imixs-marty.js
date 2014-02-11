f8b8e62a7b3b4495903b5d3b6aeb84ee60a0574c
Ralph Soika <ralph.soika@imixs.com>
2014-02-11 11:01:04 +0100

    issue #18 - some code cleanup

--- imixs-marty-util/src/main/resources/META-INF/resources/marty/imixs-marty.js
+++ imixs-marty-util/src/main/resources/META-INF/resources/marty/imixs-marty.js
@@ -9,109 +9,139 @@
  * The method also clears the input value on blur event.
  */
 $(document).ready(function() {
-	$(".marty-workitemlink-inputbox input").each(function(index, input) {
-	    var onblur = input.onblur;
-	    input.onblur = null;	
-	    $(input).on("blur", function(event) {
-	    	// clear the input on blur
-	    	$(this).val(''); 
-	        delayEvent(function() { onblur.call(input, event); }, 300);
-	    });
-	    
-	    // turn autocomplete of
-	    $(this).attr('autocomplete','off');
-	});
-
 	
-	$(".marty-userlistinput-inputbox input").each(function(index, input) {
-	    var onblur = input.onblur;
-	    input.onblur = null;	
-	    $(input).on("blur", function(event) {
-	    	// clear the input on blur
-	    	$(this).val(''); 
-	        delayEvent(function() { onblur.call(input, event); }, 300);
-	    });
-	    
-	    // turn autocomplete of
-	    $(this).attr('autocomplete','off');
-	});
-
-
-	// this is the support for the marty userinput widget
+	// this is the support for the marty input widgets
+	initWorkitemLinkInput(document.body);
+	initUserListInput(document.body);
 	initUserInput(document.body);
-	
+
 });
 
+/* 
+ * Heper Fuction for onblur delay event for suggest lists
+ */
 var delayEvent = (function() {
-	
-    var timer = 0;
-    return function(callback, timeout) {
-        clearTimeout(timer);
-        timer = setTimeout(callback, timeout);
-    };
+
+	var timer = 0;
+	return function(callback, timeout) {
+		clearTimeout(timer);
+		timer = setTimeout(callback, timeout);
+	};
 })();
 
-
-
-
-
-
+/*
+ * This method initializes all marty userInput widgets. The method can also be
+ * used in ajax events to refresh a specific form section.
+ * 
+ * @param context
+ */
 function initUserInput(context) {
 	// this is the support for the marty userinput widget
-	$(".marty-userinput-inputbox [id$=\\:username_input]",context).each(function(index, input) {
+	$(".marty-userinput-inputbox [id$=\\:username_input]", context).each(
+			function(index, input) {
 
-		// id 
-    	var inputfield_id=$(this).next('input');
-    	var inputfield_display=$(this).next('input').next('input');
-    	
-		var onblur = input.onblur;
-		input.onblur = null;	
-    	// reset the username and userId input on blur event 
-	    $(input).on("blur", function(event) {
-	    		
-	    	// reset userid to '' ? 
-	    	if ($(this).val()=='')
-	    		inputfield_id.val('');
-	    	else {
-	    		// if userid selcted then display the user display name 
-	    		if (!inputfield_id.val() == '')
-	    			$(this).val(inputfield_display.val()); 
-	    		else 
-	    			$(this).val('');
-	    	}
-	    	 delayEvent(function() { onblur.call(input, event); }, 300);
-	    	
-	    });
-	    // turn autocomplete of 
-	    $(this).attr('autocomplete','off');
-	    
-		// initialize input field with current display name 
-		if (!inputfield_id.val() == '')
-			$(this).val(inputfield_display.val());
-		
-		
-		// hide id input field 
-		inputfield_id.hide();
-		inputfield_display.hide();
-	});
-	
+				// id
+				var inputfield_id = $(this).next('input');
+				var inputfield_display = $(this).next('input').next('input');
+
+				var onblur = input.onblur;
+				input.onblur = null;
+				// reset the username and userId input on blur event
+				$(input).on("blur", function(event) {
+
+					// reset userid to '' ?
+					if ($(this).val() == '')
+						inputfield_id.val('');
+					else {
+						// if userid selcted then display the user display name
+						if (!inputfield_id.val() == '')
+							$(this).val(inputfield_display.val());
+						else
+							$(this).val('');
+					}
+					delayEvent(function() {
+						onblur.call(input, event);
+					}, 300);
+
+				});
+				// turn autocomplete of
+				$(this).attr('autocomplete', 'off');
+
+				// initialize input field with current display name
+				if (!inputfield_id.val() == '')
+					$(this).val(inputfield_display.val());
+
+				// hide id input field
+				inputfield_id.hide();
+				inputfield_display.hide();
+			});
+
 }
 
+/*
+ * This method initializes all marty userListInput widgets. The method can also
+ * be used in ajax events to refresh a specific form section.
+ * 
+ * @param context
+ */
+function initUserListInput(context) {
+	// this is the support for the marty userinput widget
+	$(".marty-userlistinput-inputbox input", context).each(
+			function(index, input) {
+				var onblur = input.onblur;
+				input.onblur = null;
+				$(input).on("blur", function(event) {
+					// clear the input on blur
+					$(this).val('');
+					delayEvent(function() {
+						onblur.call(input, event);
+					}, 300);
+				});
+
+				// turn autocomplete of
+				$(this).attr('autocomplete', 'off');
+			});
+}
+
+/*
+ * This method initializes all marty workitemLinkInput widgets. The method can
+ * also be used in ajax events to refresh a specific form section.
+ * 
+ * @param context
+ */
+function initWorkitemLinkInput(context) {
+	// this is the support for the marty userinput widget
+	$(".marty-workitemlink-inputbox input", context).each(
+			function(index, input) {
+
+				var onblur = input.onblur;
+				input.onblur = null;
+				$(input).on("blur", function(event) {
+					// clear the input on blur
+					$(this).val('');
+					delayEvent(function() {
+						onblur.call(input, event);
+					}, 300);
+				});
+
+				// turn autocomplete of
+				$(this).attr('autocomplete', 'off');
+			});
+
+}
 
 /*
  * This method updates the hidden input fields for the mary userinput widget.
- * The method expects the jquery object of the user input box
- * and the userid and displayname to upate. 
- * The method finds the input fields and updates the values.
+ * The method expects the jquery object of the user input box and the userid and
+ * displayname to upate. The method finds the input fields and updates the
+ * values.
  */
-function updateUserID(inputBox,id,displayname) {
-	 var inputfield_name=$(inputBox).find('[id$=\\:username_input]');
-	 var inputfield_id=$(inputBox).find('[id$=\\:userid_input]');
-	 var inputfield_display=$(inputfield_id).next();
-	 // alert(inputfield_id.val());
-	 inputfield_id.val(id);
-	 inputfield_name.val(displayname);
-	 inputfield_display.val(displayname);
+function updateUserID(inputBox, id, displayname) {
+	var inputfield_name = $(inputBox).find('[id$=\\:username_input]');
+	var inputfield_id = $(inputBox).find('[id$=\\:userid_input]');
+	var inputfield_display = $(inputfield_id).next();
+	// alert(inputfield_id.val());
+	inputfield_id.val(id);
+	inputfield_name.val(displayname);
+	inputfield_display.val(displayname);
 }
-
-
