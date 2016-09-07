0d304d3008ec595afe324a9bfea2ae9b6b6eadb4
Ralph Soika <ralph.soika@imixs.com>
2016-09-07 12:18:52 +0200

    fixed change event

--- imixs-marty-util/src/main/java/org/imixs/marty/workflow/ChildWorkitemController.java
+++ imixs-marty-util/src/main/java/org/imixs/marty/workflow/ChildWorkitemController.java
@@ -148,6 +148,12 @@ public class ChildWorkitemController extends org.imixs.workflow.jee.faces.workit
 				&& !workflowEvent.getWorkitem().getItemValueString("type").startsWith("workitem"))
 			return;
 
+		
+		if (WorkflowEvent.WORKITEM_CHANGED == workflowEvent
+				.getEventType()) {
+			reset();
+			
+		}
 	}
 
 	/**
