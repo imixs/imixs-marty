1e75c7ae549afb50229f22d2936fd0d5948d6ad3
Ralph Soika <ralph.soika@imixs.com>
2017-09-20 14:37:52 +0200

    typos

--- imixs-marty-util/src/main/java/org/imixs/marty/workflow/WorkflowController.java
+++ imixs-marty-util/src/main/java/org/imixs/marty/workflow/WorkflowController.java
@@ -397,11 +397,6 @@ public class WorkflowController extends org.imixs.workflow.faces.workitem.Workfl
 			// add a new FacesMessage into the FacesContext
 			ErrorHandler.handlePluginException(pe);
 		}
-//	 catch (ModelException me) {
-//		actionResult = null;
-//		// add a new FacesMessage into the FacesContext
-//		ErrorHandler.handleModelException(me);
-//	}
 
 		return actionResult;
 	}
