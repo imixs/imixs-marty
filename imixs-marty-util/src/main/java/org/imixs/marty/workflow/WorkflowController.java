c78f542e487deeffcfd03ae54e79f745118c1111
Ralph Soika <ralph.soika@imixs.com>
2017-09-20 14:37:20 +0200

    typos

--- imixs-marty-util/src/main/java/org/imixs/marty/workflow/WorkflowController.java
+++ imixs-marty-util/src/main/java/org/imixs/marty/workflow/WorkflowController.java
@@ -84,8 +84,7 @@ import org.imixs.workflow.faces.util.LoginController;
  */
 @Named
 @ConversationScoped
-public class WorkflowController extends org.imixs.workflow.faces.workitem.WorkflowController
-		implements Serializable {
+public class WorkflowController extends org.imixs.workflow.faces.workitem.WorkflowController implements Serializable {
 
 	public static final String DEFAULT_EDITOR_ID = "form_panel_simple#basic";
 	public static final String DEFAULT_ACTION_RESULT = "/pages/workitems/workitem";
@@ -142,9 +141,9 @@ public class WorkflowController extends org.imixs.workflow.faces.workitem.Workfl
 	}
 
 	/**
-	 * The method loads a new wokitem by a uniqueID. If no id is provided the
-	 * method did not change the current workitem reference. If the uniqueId is
-	 * invalid the workitem will be set to null (see setUnqiueId)
+	 * The method loads a new wokitem by a uniqueID. If no id is provided the method
+	 * did not change the current workitem reference. If the uniqueId is invalid the
+	 * workitem will be set to null (see setUnqiueId)
 	 * 
 	 * This method is used for the DeepLink Feature used by workitem.xhml.
 	 * 
@@ -186,26 +185,28 @@ public class WorkflowController extends org.imixs.workflow.faces.workitem.Workfl
 	public void load(String uniqueID) {
 		super.load(uniqueID);
 		if (conversation.isTransient()) {
-			conversation.setTimeout(((HttpServletRequest)FacesContext.getCurrentInstance().getExternalContext()
-					.getRequest()).getSession().getMaxInactiveInterval()*1000);
+			conversation.setTimeout(
+					((HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest())
+							.getSession().getMaxInactiveInterval() * 1000);
 			conversation.begin();
 			logger.fine("start new conversation, id=" + conversation.getId());
 		}
 	}
 
 	/**
-	 * This ActionListener method creates a new empty workitem. An existing
-	 * workitem and optional conversation context will be reset. The method
-	 * starts a new conversation context. Finally the method fires the
-	 * WorkfowEvent WORKITEM_CREATED.
+	 * This ActionListener method creates a new empty workitem. An existing workitem
+	 * and optional conversation context will be reset. The method starts a new
+	 * conversation context. Finally the method fires the WorkfowEvent
+	 * WORKITEM_CREATED.
 	 */
 	@Override
 	public void create(ActionEvent event) {
 		super.create();
 
 		if (conversation.isTransient()) {
-			conversation.setTimeout(((HttpServletRequest)FacesContext.getCurrentInstance().getExternalContext()
-					.getRequest()).getSession().getMaxInactiveInterval()*1000);
+			conversation.setTimeout(
+					((HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest())
+							.getSession().getMaxInactiveInterval() * 1000);
 			conversation.begin();
 			logger.fine("start new conversation, id=" + conversation.getId());
 		}
@@ -215,17 +216,17 @@ public class WorkflowController extends org.imixs.workflow.faces.workitem.Workfl
 	}
 
 	/**
-	 * This method creates a new empty workitem. An existing workitem and
-	 * optional conversation context will be reset.
+	 * This method creates a new empty workitem. An existing workitem and optional
+	 * conversation context will be reset.
 	 * 
 	 * The method assigns the initial values '$ModelVersion', '$ProcessID' and
 	 * '$UniqueIDRef' to the new workitem. The method creates the empty field
-	 * '$workitemID' and the field 'namowner' which is assigned to the current
-	 * user. This data can be used in case that a workitem is not processed but
-	 * saved (e.g. by the dmsController).
+	 * '$workitemID' and the field 'namowner' which is assigned to the current user.
+	 * This data can be used in case that a workitem is not processed but saved
+	 * (e.g. by the dmsController).
 	 * 
-	 * The method starts a new conversation context. Finally the method fires
-	 * the WorkfowEvent WORKITEM_CREATED.
+	 * The method starts a new conversation context. Finally the method fires the
+	 * WorkfowEvent WORKITEM_CREATED.
 	 * 
 	 * @param modelVersion
 	 *            - model version
@@ -263,8 +264,9 @@ public class WorkflowController extends org.imixs.workflow.faces.workitem.Workfl
 
 		// start now the new conversation
 		if (conversation.isTransient()) {
-			conversation.setTimeout(((HttpServletRequest)FacesContext.getCurrentInstance().getExternalContext()
-					.getRequest()).getSession().getMaxInactiveInterval()*1000);
+			conversation.setTimeout(
+					((HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest())
+							.getSession().getMaxInactiveInterval() * 1000);
 			conversation.begin();
 			logger.fine("start new conversation, id=" + conversation.getId());
 		}
@@ -289,15 +291,14 @@ public class WorkflowController extends org.imixs.workflow.faces.workitem.Workfl
 	}
 
 	/**
-	 * The action method processes the current workItem and fires the
-	 * WorkflowEvents WORKITEM_BEFORE_PROCESS and WORKITEM_AFTER_PROCESS. The
-	 * Method also catches PluginExceptions and adds the corresponding Faces
-	 * Error Message into the FacesContext. In case of an exception the
-	 * WorkflowEvent WORKITEM_AFTER_PROCESS will not be fired. <br>
-	 * The action result returned by the workflow engine may contain a $uniqueid
-	 * to redirect the user and load that new workitem. If no action result is
-	 * defined the method redirects to the default action with the current
-	 * workitem id.
+	 * The action method processes the current workItem and fires the WorkflowEvents
+	 * WORKITEM_BEFORE_PROCESS and WORKITEM_AFTER_PROCESS. The Method also catches
+	 * PluginExceptions and adds the corresponding Faces Error Message into the
+	 * FacesContext. In case of an exception the WorkflowEvent
+	 * WORKITEM_AFTER_PROCESS will not be fired. <br>
+	 * The action result returned by the workflow engine may contain a $uniqueid to
+	 * redirect the user and load that new workitem. If no action result is defined
+	 * the method redirects to the default action with the current workitem id.
 	 * 
 	 * The method appends faces-redirect=true to the action result in case no
 	 * faces-redirect is defined.
@@ -308,9 +309,10 @@ public class WorkflowController extends org.imixs.workflow.faces.workitem.Workfl
 	 * </code>
 	 * 
 	 * In case the processing was successful, the current conversation will be
-	 * closed. In Case of an Exception (e.g PluginException) the conversation
-	 * will not be closed, so that the current workitem data is still available.
-	 * @throws ModelException 
+	 * closed. In Case of an Exception (e.g PluginException) the conversation will
+	 * not be closed, so that the current workitem data is still available.
+	 * 
+	 * @throws ModelException
 	 * 
 	 */
 	@Override
@@ -323,8 +325,8 @@ public class WorkflowController extends org.imixs.workflow.faces.workitem.Workfl
 			// fire event
 			long l1 = System.currentTimeMillis();
 			events.fire(new WorkflowEvent(getWorkitem(), WorkflowEvent.WORKITEM_BEFORE_PROCESS));
-			logger.finest("[process] fire WORKITEM_BEFORE_PROCESS event: ' in "
-					+ (System.currentTimeMillis() - l1) + "ms");
+			logger.finest(
+					"[process] fire WORKITEM_BEFORE_PROCESS event: ' in " + (System.currentTimeMillis() - l1) + "ms");
 
 			// process workitem
 			actionResult = super.process();
@@ -338,8 +340,8 @@ public class WorkflowController extends org.imixs.workflow.faces.workitem.Workfl
 			// fire event
 			long l2 = System.currentTimeMillis();
 			events.fire(new WorkflowEvent(getWorkitem(), WorkflowEvent.WORKITEM_AFTER_PROCESS));
-			logger.finest("[process] fire WORKITEM_AFTER_PROCESS event: ' in "
-					+ (System.currentTimeMillis() - l2) + "ms");
+			logger.finest(
+					"[process] fire WORKITEM_AFTER_PROCESS event: ' in " + (System.currentTimeMillis() - l2) + "ms");
 
 			// if a action was set by the workflowController, then this
 			// action will be the action result String
@@ -368,9 +370,8 @@ public class WorkflowController extends org.imixs.workflow.faces.workitem.Workfl
 			logger.fine("action result=" + actionResult);
 
 			if (logger.isLoggable(Level.FINEST)) {
-				logger.finest(
-						"[process] '" + getWorkitem().getItemValueString(WorkflowService.UNIQUEID)
-								+ "' completed in " + (System.currentTimeMillis() - lTotal) + "ms");
+				logger.finest("[process] '" + getWorkitem().getItemValueString(WorkflowService.UNIQUEID)
+						+ "' completed in " + (System.currentTimeMillis() - lTotal) + "ms");
 			}
 
 			// close conversation
@@ -396,6 +397,11 @@ public class WorkflowController extends org.imixs.workflow.faces.workitem.Workfl
 			// add a new FacesMessage into the FacesContext
 			ErrorHandler.handlePluginException(pe);
 		}
+//	 catch (ModelException me) {
+//		actionResult = null;
+//		// add a new FacesMessage into the FacesContext
+//		ErrorHandler.handleModelException(me);
+//	}
 
 		return actionResult;
 	}
@@ -406,8 +412,8 @@ public class WorkflowController extends org.imixs.workflow.faces.workitem.Workfl
 	 * 
 	 * NOTE: the super class changes the behavior of save(action) and process a
 	 * workItem instead of saving. This may conflict in future cases. If so we
-	 * should decide if we simply add a new method here called 'saveAsDraft()'
-	 * which would more precisely describe the behavior of this method.
+	 * should decide if we simply add a new method here called 'saveAsDraft()' which
+	 * would more precisely describe the behavior of this method.
 	 */
 	@Override
 	public void save() throws AccessDeniedException {
@@ -456,8 +462,7 @@ public class WorkflowController extends org.imixs.workflow.faces.workitem.Workfl
 	}
 
 	/**
-	 * Returns the current uniqueid of the workitem or null if no workitem is
-	 * set.
+	 * Returns the current uniqueid of the workitem or null if no workitem is set.
 	 * 
 	 * @return uniqueid of current workitem or null
 	 */
@@ -470,14 +475,14 @@ public class WorkflowController extends org.imixs.workflow.faces.workitem.Workfl
 	}
 
 	/**
-	 * returns the workflowEditorID for the current workItem. If no attribute
-	 * with the name "txtWorkflowEditorid" is available then the method return
-	 * the DEFAULT_EDITOR_ID.
+	 * returns the workflowEditorID for the current workItem. If no attribute with
+	 * the name "txtWorkflowEditorid" is available then the method return the
+	 * DEFAULT_EDITOR_ID.
 	 * 
-	 * Additional the method tests if the txtWorkflowEditorid contains the
-	 * character '#'. This character indicates additional form-section
-	 * informations. The Method cuts this information and provides an Array of
-	 * EditoSection Objects by the property EditorSections
+	 * Additional the method tests if the txtWorkflowEditorid contains the character
+	 * '#'. This character indicates additional form-section informations. The
+	 * Method cuts this information and provides an Array of EditoSection Objects by
+	 * the property EditorSections
 	 * 
 	 * @see getEditorSections
 	 * 
@@ -502,21 +507,21 @@ public class WorkflowController extends org.imixs.workflow.faces.workitem.Workfl
 	}
 
 	/**
-	 * returns an array list with EditorSection Objects. Each EditorSection
-	 * object contains the url and the name of one section. EditorSections can
-	 * be provided by the workitem property 'txtWorkflowEditorid' marked with
-	 * the '#' character and separated with charater '|'.
+	 * returns an array list with EditorSection Objects. Each EditorSection object
+	 * contains the url and the name of one section. EditorSections can be provided
+	 * by the workitem property 'txtWorkflowEditorid' marked with the '#' character
+	 * and separated with charater '|'.
 	 * 
 	 * e.g.: form_tab#basic_project|sub_timesheet[owner,manager]
 	 * 
 	 * This example provides the editor sections 'basic_project' and
-	 * 'sub_timesheet'. The optional marker after the second section in []
-	 * defines the user membership to access this action. In this example the
-	 * second section is only visible if the current user is member of the
-	 * project owner or manager list.
+	 * 'sub_timesheet'. The optional marker after the second section in [] defines
+	 * the user membership to access this action. In this example the second section
+	 * is only visible if the current user is member of the project owner or manager
+	 * list.
 	 * 
-	 * The following example illustrates how to iterate over the section array
-	 * from a JSF fragment:
+	 * The following example illustrates how to iterate over the section array from
+	 * a JSF fragment:
 	 * 
 	 * <code>
 	 * <ui:repeat value="#{workitemMB.editorSections}" var="section">
@@ -525,9 +530,9 @@ public class WorkflowController extends org.imixs.workflow.faces.workitem.Workfl
 	 * </code>
 	 * 
 	 * 
-	 * The array of EditorSections also contains information about the name for
-	 * a section. This name is read from the resouce bundle 'bundle.forms'. The
-	 * '/' character will be replaced with '_'. So for example the section url
+	 * The array of EditorSections also contains information about the name for a
+	 * section. This name is read from the resouce bundle 'bundle.forms'. The '/'
+	 * character will be replaced with '_'. So for example the section url
 	 * myforms/sub_timesheet will result in resoure bundle lookup for the name
 	 * 'myforms_sub_timersheet'
 	 * 
@@ -642,9 +647,9 @@ public class WorkflowController extends org.imixs.workflow.faces.workitem.Workfl
 	}
 
 	/**
-	 * this method loads all versions to the current workitem. Idependent from
-	 * the type property! The method returns an empty list if no version exist
-	 * (only the main version)
+	 * this method loads all versions to the current workitem. Idependent from the
+	 * type property! The method returns an empty list if no version exist (only the
+	 * main version)
 	 * 
 	 * @see org.imixs.WorkitemService.business.WorkitemServiceBean
 	 */
@@ -654,17 +659,13 @@ public class WorkflowController extends org.imixs.workflow.faces.workitem.Workfl
 			return;
 		List<ItemCollection> col = null;
 		String sRefID = getWorkitem().getItemValueString("$workitemId");
-//		String refQuery = "SELECT entity FROM Entity entity " + " JOIN entity.textItems AS t"
-//				+ "  WHERE entity.type IN ('workitem', 'workitemarchive', 'workitemversion') "
-//				+ "  AND t.itemName = '$workitemid'" + "  AND t.itemValue = '" + sRefID + "' "
-//				+ " ORDER BY entity.modified ASC";
-
-		String refQuery="( (type:\"workitem\" OR type:\"workitemarchive\" OR type:\"workitemversion\") AND $workitemid:\""+sRefID + "\")";
+		String refQuery = "( (type:\"workitem\" OR type:\"workitemarchive\" OR type:\"workitemversion\") AND $workitemid:\""
+				+ sRefID + "\")";
 		try {
-			col = this.getWorkflowService().getDocumentService().find(refQuery, 999,0);
+			col = this.getWorkflowService().getDocumentService().find(refQuery, 999, 0);
 			// sort by $modified
 			Collections.sort(col, new ItemCollectionComparator("$modified", true));
-			
+
 			// Only return version list if more than one version was found!
 			if (col.size() > 1) {
 				for (ItemCollection aworkitem : col) {
