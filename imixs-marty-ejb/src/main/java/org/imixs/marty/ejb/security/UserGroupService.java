a4c4b33aa08d72fd9c97b2f9a1a0a9016544f6e5
Ralph Soika <ralph.soika@imixs.com>
2018-11-24 14:05:57 +0100

    implementation
    issue #278

--- imixs-marty-ejb/src/main/java/org/imixs/marty/ejb/security/UserGroupService.java
+++ imixs-marty-ejb/src/main/java/org/imixs/marty/ejb/security/UserGroupService.java
@@ -24,6 +24,7 @@ package org.imixs.marty.ejb.security;
 
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.logging.Logger;
 
@@ -56,8 +57,11 @@ import org.imixs.workflow.exceptions.AccessDeniedException;
 @LocalBean
 public class UserGroupService {
 
-	public static String ACCESSLEVEL_NOACCESS="org.imixs.ACCESSLEVEL.NOACCESS";
-	
+	public static String ACCESSLEVEL_NOACCESS = "org.imixs.ACCESSLEVEL.NOACCESS";
+
+	public static String DEFAULT_ACCOUNT = "admin";
+	public static String DEFAULT_PASSWORD = "adminadmin";
+
 	@PersistenceContext(unitName = "org.imixs.workflow.jpa")
 	private EntityManager manager;
 
@@ -106,7 +110,7 @@ public class UserGroupService {
 			// remove password....
 			profile.removeItem("txtPassword");
 			logger.info("password change for userid '" + sID + "' by '" + ctx.getCallerPrincipal().getName() + "'");
-
+			profile.replaceItemValue("txtpasswordhash", sEncryptedPasswort);
 		}
 
 		// find group relation ships
@@ -124,9 +128,8 @@ public class UserGroupService {
 			}
 		}
 
-		
 		// if grouplist is empty we set the role 'org.imixs.ACCESSLEVEL.NOACCESS'
-		if (groupList.size()==0) {
+		if (groupList.size() == 0) {
 			// verify if no access exists...
 			UserGroup noAccessGroup = manager.find(UserGroup.class, ACCESSLEVEL_NOACCESS);
 			// if group dos not exist - create it...
@@ -136,8 +139,7 @@ public class UserGroupService {
 			}
 			groupList.add(noAccessGroup);
 		}
-		
-		
+
 		// update groups
 		user.setUserGroups(groupList);
 
@@ -219,17 +221,17 @@ public class UserGroupService {
 	 */
 	@SuppressWarnings("unchecked")
 	public void initUserIDs() {
-		
+
 		String setupMode = propertyService.getProperties().getProperty("setup.mode", "auto");
 		if (!"auto".equalsIgnoreCase(setupMode)) {
 			// init userdb is disabled
 			logger.finest("...... initUserIDs is disabled");
 			return;
 		}
-		
-		logger.info("init default UserIDs...");
+
+		logger.finest("......init UserIDs...");
 		// default admin account
-		String sAdminAccount = "admin";
+		String sAdminAccount = DEFAULT_ACCOUNT;
 		String userInputMode = propertyService.getProperties().getProperty("security.userid.input.mode",
 				ProfilePlugin.DEFAULT_USER_INPUT_MODE);
 
@@ -246,13 +248,13 @@ public class UserGroupService {
 		Collection<UserId> entityList = q.getResultList();
 
 		if (entityList == null || entityList.size() == 0) {
-			logger.info("Create default admin account");
+			logger.info("Create default admin account...");
 			// create a default account
 			ItemCollection profile = new ItemCollection();
 			profile.replaceItemValue("type", "profile");
 			profile.replaceItemValue("txtName", sAdminAccount);
 			// set default password
-			profile.replaceItemValue("txtPassword", "adminadmin");
+			profile.replaceItemValue("txtPassword", DEFAULT_PASSWORD);
 			profile.replaceItemValue("txtWorkflowGroup", "Profile");
 			profile.replaceItemValue("txtGroups", "IMIXS-WORKFLOW-Manager");
 
@@ -264,6 +266,7 @@ public class UserGroupService {
 			try {
 				updateUser(profile);
 				documentService.save(profile);
+				verifyExistingProfileData();
 			} catch (AccessDeniedException e) {
 				logger.warning("UserGroupService - unable to initialize default admin account");
 				logger.severe(e.getMessage());
@@ -275,4 +278,71 @@ public class UserGroupService {
 
 	}
 
+	/**
+	 * This method is called by the initUserIDs method in case an admin profile was
+	 * not found.
+	 * <p>
+	 * The method trys to update the userdata based on existing profiles data and
+	 * restore groups and encrypted passwords.
+	 * 
+	 */
+	@SuppressWarnings("unchecked")
+	private void verifyExistingProfileData() {
+		logger.info("Verify existing profile data...");
+		List<ItemCollection> profiles = documentService.getDocumentsByType("profile");
+		for (ItemCollection profile : profiles) {
+
+			String id = profile.getItemValueString("txtname");
+
+			// try to update data.....
+			UserId user = null;
+			user = manager.find(UserId.class, id);
+			if (user != null) {
+				continue;
+			}
+
+			// userobject does not exist, so we restore the user object based on the
+			// existing profile data if the profile contains a password hash....
+			String passwordhash = profile.getItemValueString("txtpasswordhash");
+			if (!passwordhash.isEmpty()) {
+				logger.info("...restore userid '" + id + "' from existing profile data...");
+				user = new UserId(id);
+				user.setPassword(passwordhash);
+				manager.persist(user);
+
+				// find group relation ships
+				Collection<String> groups = profile.getItemValue("txtGroups");
+				Set<UserGroup> groupList = new HashSet<UserGroup>();
+				for (String aGroup : groups) {
+					// we do not except empty groupnames here!
+					if (aGroup != null && !aGroup.isEmpty()) {
+						UserGroup group = manager.find(UserGroup.class, aGroup);
+						// if group dos not exist - create it...
+						if (group == null) {
+							group = new UserGroup(aGroup);
+							manager.persist(group);
+						}
+						groupList.add(group);
+					}
+				}
+
+				// if grouplist is empty we set the role 'org.imixs.ACCESSLEVEL.NOACCESS'
+				if (groupList.size() == 0) {
+					// verify if no access exists...
+					UserGroup noAccessGroup = manager.find(UserGroup.class, ACCESSLEVEL_NOACCESS);
+					// if group dos not exist - create it...
+					if (noAccessGroup == null) {
+						noAccessGroup = new UserGroup(ACCESSLEVEL_NOACCESS);
+						manager.persist(noAccessGroup);
+					}
+					groupList.add(noAccessGroup);
+				}
+
+				// update groups
+				user.setUserGroups(groupList);
+			}
+
+		}
+
+	}
 }
